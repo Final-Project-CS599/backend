157f09a42510d62fa05e97910cfabf55d9925861
FatmaSalama219 <fatmasalama2191997@gmail.com>
2024-12-20 21:39:02 +0200

    Update createMedia.js

--- src/DB/models/instructors/createMedia.js
+++ src/DB/models/instructors/createMedia.js
@@ -2,7 +2,7 @@ export const createMedia =
 `CREATE TABLE IF NOT EXISTS media (
     m_type VARCHAR(200) NOT NULL,
     m_content_id INT ,
-    FOREIGN KEY (m_content_id) REFERENCE content(c_id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (m_content_id) REFERENCES content(c_id) ON DELETE CASCADE ON UPDATE CASCADE
 
       
 
