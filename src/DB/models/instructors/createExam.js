982fc60d6ddaff9a21b68d91a310014bc27de991
FatmaSalama219 <fatmasalama2191997@gmail.com>
2024-12-20 21:38:25 +0200

    Update createExam.js

--- src/DB/models/instructors/createExam.js
+++ src/DB/models/instructors/createExam.js
@@ -4,7 +4,7 @@ export const createExamTable =
         e_link VARCHAR(200) NOT NULL,
         e_degree FLOAT NOT NULL, 
         e_content_id INT ,
-        FOREIGN KEY (e_content_id) REFERENCE content(c_id) ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (e_content_id) REFERENCES content(c_id) ON DELETE CASCADE ON UPDATE CASCADE
 
 
         );` 
