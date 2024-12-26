f16a00cbcdb9b57a530c5541d787348a0e7b767d
Aya Farouk <abdelwahab.aya@gmail.com>
2024-12-26 14:34:51 +0200

    constrain

--- src/DB/models/instructors/createtblinstructor.js
+++ src/DB/models/instructors/createtblinstructor.js
@@ -5,7 +5,7 @@ export const instructorTable =  `CREATE TABLE IF NOT EXISTS Instructors (
     i_email VARCHAR(255) UNIQUE,
     i_password VARCHAR(200),
     i_departmentId INT,
-    FOREIGN KEY (i_departmentId) REFERENCES department(d_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (i_departmentId) REFERENCES department(d_id) ON DELETE SET NULL ON UPDATE CASCADE,
     i_adminId VARCHAR(14),
     FOREIGN KEY (i_adminId) REFERENCES superAdmin(sAdmin_nationalID) ON DELETE SET NULL,
     i_createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
