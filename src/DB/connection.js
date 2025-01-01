42e057877cb98bd93002b7c1acb599bd3d73f150
IsraaGamalAdel <esraagamal13258@gmail.com>
2025-01-01 22:04:41 +0200

    Update BD Connection

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -5,10 +5,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve('.env.dev') });
 
 const dbConfig = mysql2.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host     : process.env.MYSQL_ADDON_HOST,
+  database : process.env.MYSQL_ADDON_DB,
+  user     : process.env.MYSQL_ADDON_USER,
+  password : process.env.MYSQL_ADDON_PASSWORD
 });
 
+
 export default dbConfig;
+
