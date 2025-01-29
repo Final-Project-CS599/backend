5ab76f4c90245b1b4c1ed390e54d91214d7e1c14
Aya Farouk <abdelwahab.aya@gmail.com>
2025-01-29 23:40:35 +0200

    fixed conflicts in app.controller.js and updateDB.controller.js

--- src/modules/admin/updateDB/updateDB.controller.js
+++ src/modules/admin/updateDB/updateDB.controller.js
@@ -3,14 +3,8 @@ import * as usersServices from "./service/updateDB.service.js";
 
 const router = Router();
 
-<<<<<<< HEAD
-router.get('/table-student' ,usersServices.alterTableIStudent);
-router.get('/table-SuperAdmin' , usersServices.alterTableAdmin);
-// router.get('/truncate-tables' , usersServices.truncateTables);
-export default router;
-=======
 router.get("/table-student", usersServices.alterTableIStudent);
 router.get("/table-SuperAdmin", usersServices.alterTableAdmin);
 // router.get('/truncate-tables' , usersServices.truncateTables);
 export default router;
->>>>>>> 48039b54842b495017d07f2a3f26e41eb250fc29
+
--- src/modules/app.controller.js
+++ src/modules/app.controller.js
@@ -11,36 +11,27 @@ import departmentsRouter from './admin/department/department.routes.js';
 import editStudentsRouter from "./admin/editStudent/editStudent.routes.js";
 import adminProfileRouter from "./admin/adminProfile/adminProfile.routes.js";
 // Student controllers
-<<<<<<< HEAD
-import userRoutes from '../modules/student/users/routes.js';
+import userRoutes from "../modules/student/users/routes.js";
 import updateStudentProfileRoutes from '../modules/student/profile/routes.js';
 import studentHelpDeskRoutes from '../modules/student/helpDesk/routes.js';
 import { updateInstructorProfile } from './instructor/profile/ProfInst.services.js';
 import uploadCourseMaterial from './instructor/courses/upload.controller.js';
 import { viewMaterialCourse } from './instructor/courses/viewcourseMat/view.services.js'; 
-import  examController from "./instructor/exam/exam.controller.js"
-import  MessageController from "./instructor/message/message.controller.js"
-import contentController from "./instructor/content/content.controller.js"
+import  examController from "./instructor/exam/exam.controller.js";
+import  MessageController from "./instructor/message/message.controller.js";
+import contentController from "./instructor/content/content.controller.js";
 import assignmentController from "./instructor/Assignment/Assignment.controller.js"
+import instructorRout from "../modules/student/Instructores/instructor.route.js";
+import assignmentRout from "../modules/student/Assinment/assign.route.js";
 // Cors 
 import cors  from 'cors' 
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
-=======
-import userRoutes from "../modules/student/users/routes.js";
-import updateStudentProfileRoutes from "../modules/student/profile/routes.js";
-import studentHelpDeskRoutes from "../modules/student/helpDesk/routes.js";
-import uploadCourseMaterial from "./instructor/courses/upload.controller.js";
-import { viewMaterialCourse } from "./instructor/courses/viewcourseMat/view.services.js";
-import examController from "../modules/instructor/exam/exam.controller.js";
-import MessageController from "../modules/instructor/message/message.controller.js";
-import instructorRout from "../modules/student/Instructores/instructor.route.js";
-import assignmentRout from "../modules/student/Assinment/assign.route.js";
-// Cors
-import cors from "cors";
-import session from "express-session";
-import cookieParser from "cookie-parser";
->>>>>>> 48039b54842b495017d07f2a3f26e41eb250fc29
+
+
+
+
+
 
 
 
@@ -101,7 +92,6 @@ const bootstrap = (app, express) => {
   app.use(`${baseUrl}/users`, userRoutes);
   app.use(`${baseUrl}/student`, updateStudentProfileRoutes);
   app.use(`${baseUrl}/student`, studentHelpDeskRoutes);
-<<<<<<< HEAD
   app.use('/courseMaterial', uploadCourseMaterial);
   app.use('/courseMaterial', viewMaterialCourse);
   app.use('/exam', examController)
@@ -109,23 +99,11 @@ const bootstrap = (app, express) => {
   app.use('/assignment',assignmentController)
   app.use('/content',contentController)
   app.use('/instProfile',updateInstructorProfile)
-  
-  app.all('*', (req, res, next) => {
-    return res.status(404).json({ message: 'In-valid routing' });
-=======
-  app.use("/courseMaterial", uploadCourseMaterial);
-  app.use("/courseMaterial", viewMaterialCourse);
-  app.use("/exam", examController);
-  app.use("/message", MessageController);
   app.use(`${baseUrl}/departments`, departmentsRouter);
   app.use(`${baseUrl}/adminProfile`, adminProfileRouter);
   app.use(`${baseUrl}/student/instructor`, instructorRout);
   app.use(`${baseUrl}/student`, assignmentRout);
   app.use(`${baseUrl}/editStudents`, editStudentsRouter);
-
-  app.all("*", (req, res, next) => {
-    return res.status(404).json({ message: "In-valid routing" });
-  });
 
   app.get("/", (req, res, next) => {
     return res.status(200).json({
@@ -135,7 +113,6 @@ const bootstrap = (app, express) => {
 
   app.all("*", (req, res, next) => {
     return res.status(404).json({ message: "In-valid routing" });
->>>>>>> 48039b54842b495017d07f2a3f26e41eb250fc29
   });
 
   app.use(globalErrorHandling);
