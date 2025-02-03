0b0b7708681e158eab0f972b880a06616c4e630b
fatma salama <fatma salama2191997@gmail.com>
2025-02-03 02:02:36 +0200

    salamaa

--- src/DB/models/student/createAssignmentTable.js
+++ src/DB/models/student/createAssignmentTable.js
@@ -1,40 +1,12 @@
-import dbConfig from "../../../DB/connection.js";
-
-export const addAssignment = async (req, res, next) => {
-    try {
-        const instructor_id = req.user.id; 
-        const { title, description, degree, type, link } = req.body;
-
-        if (!instructor_id || !title || !description || !degree || !type || !link) {
-            return res.status(400).json({ message: 'All fields are required' });
-        }
-
-        const courseQuery = `SELECT c_id FROM courses WHERE instructor_id = ? ORDER BY c_id DESC LIMIT 1`;
-        dbConfig.query(courseQuery, [instructor_id], (err, result) => {
-            if (err || result.length === 0) {
-                return res.status(400).json({ message: "No course found for this instructor" });
-            }
-
-            const courseId = result[0].c_id; 
-            const insertQuery = `INSERT INTO assignment (a_title, a_description, a_degree, a_type, a_link, a_instructor_id, a_courseId, a_publish_date)
-                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-
-            dbConfig.query(insertQuery, [title, description, degree, type, link, instructor_id, courseId], (err, result) => {
-                if (err) {
-                    return res.status(500).json({ 
-                        message: "Failed to execute query", 
-                        error: err.message 
-                    });
-                }
-                return res.status(201).json({ message: "Assignment added successfully", assignmentId: result.insertId });
-            });
-        });
-
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
-};
-
+export const AssignmentTable = `CREATE TABLE IF NOT EXISTS assignment(
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    degree FLOAT NOT NULL, 
+    link VARCHAR(255) NOT NULL, 
+    content_id INT NOT NULL,
+    FOREIGN KEY (content_id) REFERENCES content(c_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+     )`;
 
 export const TakesAssignmentTable = `CREATE TABLE IF NOT EXISTS takes_assignment(
   ta_id INT AUTO_INCREMENT PRIMARY KEY,
