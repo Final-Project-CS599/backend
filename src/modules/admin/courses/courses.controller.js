590ea4596f588c4eb7d3dfe2cd98b73dcbb667b0
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2025-02-03 20:15:46 +0200

    Add payment approval endpoint and implement enrollment update logic

--- src/modules/admin/courses/courses.controller.js
+++ src/modules/admin/courses/courses.controller.js
@@ -4,6 +4,7 @@ import { validation } from '../../../middleware/validation.middleware.js';
 import * as coursesServices from './service/courses.service.js';
 import * as academicServices from './service/academic.service.js';
 import * as extraServices from './service/extraCourse.service.js';
+import * as paymentService from '../../student/courses/controller.js';
 import getAllDepartment from './service/getDepartment.service.js';
 import { verifyToken } from '../../../middleware/auth.js';
 
@@ -31,5 +32,7 @@ router.get('/getAllDepartment', getAllDepartment);
 router.post('/addExtra', extraServices.addExtra);
 router.patch('/updateExtra', extraServices.updateExtra);
 router.get('/getAllCoursesExtra', extraServices.getAllCoursesExtra);
+
+router.post('/approvePayment', verifyToken, paymentService.enrollInCourse);
 
 export default router;
