572cf0185c109153a3a6e04ab58a069bb9aac4d3
Nadamohamedabdelmaebod <nm202020180213@gmail.com>
2025-02-06 06:16:34 +0200

    delete dashboard

--- src/modules/admin/dashboard/dashboard.controller.js
+++
@@ -1,7 +0,0 @@
-// import { Router } from 'express';
-// import * as dashboardService from './service/dashboard.service.js'; 
-
-
-// const router = Router();
-// router.get('/getDashboardStats', getDashboardStats);
-// export default router;
